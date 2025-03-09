05c281a052ee2b2d13c81b12f242d750b532346d
webcoderz <19884161+webcoderz@users.noreply.github.com>
2025-03-09 19:04:40 -0400

    Update openai.py

--- pydantic_ai_slim/pydantic_ai/models/openai.py
+++ pydantic_ai_slim/pydantic_ai/models/openai.py
@@ -319,6 +319,29 @@ class OpenAIModel(Model):
             _timestamp=datetime.fromtimestamp(first_chunk.created, tz=timezone.utc),
         )
 
+<<<<<<< Updated upstream
+=======
+    @staticmethod
+    def _map_tool_choice(
+        model_settings: OpenAIModelSettings,
+        model_request_parameters: ModelRequestParameters,
+        tools: list[chat.ChatCompletionToolParam],
+    ) -> ChatCompletionToolChoiceOptionParam | None:
+        """Determine the `tool_choice` setting for the model."""
+        tool_choice = model_settings.get('tool_choice', 'auto')
+
+        if tool_choice == 'auto' and tools and not model_request_parameters.allow_text_result:
+            return 'required'
+        elif tool_choice == 'auto':
+            return None
+        elif tool_choice in ('none', 'required'):
+            return cast(ChatCompletionToolChoiceOptionParam, tool_choice)
+        elif isinstance(tool_choice, ForcedFunctionToolChoice):
+            return {'type': 'function', 'function': {'name': tool_choice.name}}
+        else:
+            assert_never(tool_choice)
+
+>>>>>>> Stashed changes
     def _get_tools(self, model_request_parameters: ModelRequestParameters) -> list[chat.ChatCompletionToolParam]:
         tools = [self._map_tool_definition(r) for r in model_request_parameters.function_tools]
         if model_request_parameters.result_tools:
